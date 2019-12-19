8af585d29ed0e9982b9679b699fc91bc421ace24
DavidSir <1135130110@qq.com>
2019-12-19 17:06:22 +0800

    java---file类学习 1

--- src/Lfile.java
+++ src/Lfile.java
@@ -6,5 +6,16 @@ public class Lfile {
         System.out.println(f.getAbsolutePath()); // 获取file绝对地址
         System.out.println(f.getCanonicalPath()); // 获取file绝对地址的规范地址
         System.out.println(f.separator); // 获取当前平台的系统分隔符
+        System.out.println(f.isFile()); // 判断该File对象是否是一个已存在的文件
+        System.out.println(f.isDirectory()); // 断该File对象是否是一个已存在的目录
+        System.out.println(f.canRead()); // 断该File对象是否是一个已存在的目录
+        System.out.println(f.canWrite()); // 断该File对象是否是一个已存在的目录
+        System.out.println(f.canExecute()); // 断该File对象是否是一个已存在的目录
+    }
+    public void createFile() throws IOException {
+        /*File createFile = new File("./../statics/newFile.txt");
+        if (createFile.createNewFile()) {
+            System.out.println("成功");
+        }*/
     }
 }
