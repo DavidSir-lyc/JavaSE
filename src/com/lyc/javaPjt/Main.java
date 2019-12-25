fdcdce2fdcceb3773b3ff59db66b70228cbaa396
DavidSir <1135130110@qq.com>
2019-12-25 15:57:34 +0800

    java---String类 学习 3

--- src/com/lyc/javaPjt/Main.java
+++ src/com/lyc/javaPjt/Main.java
@@ -6,21 +6,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         /**file类的学习*/
         LFile lfile= new LFile();
-        lfile.fileFun();
-        lfile.createFile();
+        // lfile.fileFun();
+        // lfile.createFile();
         /**
          * thread类的学习
          * 启动一个新的线程，不是直接调用 Thread  子类对象的 run() 方法，而是调用 Thread 子类的 start() 方法
          * Thread 类的 start() 方法会产生一个新的线程，该线程用于执行 Thread 子类的 run() 方法。
          * */
         LThread lthread = new LThread();
-        lthread.start(); // 开启子线程
+        // lthread.start(); // 开启子线程
         /**Runnable接口实现学习*/
         LRunnable lrunnable = new LRunnable();
         Thread thread = new Thread(lrunnable);
-        thread.start(); // 开启子线程
+        // thread.start(); // 开启子线程
         /**String类学习*/
         LString lstring = new LString();
         lstring.testString();
+        // lstring.testStringBuilder();
     }
 }
