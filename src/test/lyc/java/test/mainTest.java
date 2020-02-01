d456fd8485c38af8a3aa75b60f092b77fa721643
Administrator <135130110@qq.com>
2020-02-01 14:30:21 +0800

    java---junit和before，after 学习2

--- src/test/lyc/java/test/mainTest.java
+++ src/test/lyc/java/test/mainTest.java
@@ -1,14 +1,28 @@
 package lyc.java.test;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class mainTest {
+    // 初始化方法，所有测试方法执行前，先执行
+    @Before
+    public void init() {
+        System.out.println("init...");
+    }
+
     @Test
     public void addTest() {
         int num = 1;
         num++;
         // 测试方法中用断言，输出不靠谱
         Assert.assertEquals(2,num);
+    }
+
+    // 最终方法，所有测试方法执行后，最后执行
+    @After
+    public void close() {
+        System.out.println("close...");
     }
 }
